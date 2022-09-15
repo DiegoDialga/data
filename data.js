4545f4154e53dabc85f2d69b7b55a20a0e946c52
Deepanshu Singh Negi <deepanshunegi10@gmail.com>
2022-09-15 14:13:26 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,63 @@
+export default [
+    {
+        course: 'BCA',
+        name: 'Deepanshu',
+        ID: '21041329',
+        semester: 3,
+
+    },
+    {
+        course: 'BCA',
+        name: 'Kuldeep',
+        ID: '21041350',
+        semester: 3
+    },
+    {
+        course: 'BCA',
+        name: 'Nishant',
+        ID: '20141360',
+        semester: 3
+    },
+    {
+        course: 'BCA',
+        name: 'Harvinder',
+        ID: '21041355',
+        semester: 3
+    },
+    {
+        course: 'BCA',
+        name: 'Harnish',
+        ID: '21041335',
+        semester: 3
+    },
+    {
+        course: 'BCA',
+        name: 'Manish',
+        ID: '21041340',
+        semester: 3
+    },
+    {
+        course: 'BCA',
+        name: 'Deepak',
+        ID: '21041355',
+        semester: 3
+    },
+    {
+        course: 'BCA',
+        name: 'kartik',
+        ID: '21041385',
+        semester: 3
+    },
+    {
+        course: 'BCA',
+        name: 'karan',
+        ID: '21041350',
+        semester: 3
+    },
+    {
+        course: 'BCA',
+        name: 'Gaurav',
+        ID: '21041357',
+        semester: 3
+    },
+]
