7ec87036ebfcd8c9598832dceb8dad63866ca564
Deepanshu Singh Negi <deepanshunegi10@gmail.com>
2022-09-15 14:26:52 +0530

    Add files via upload

--- data.js
+++ data.js
@@ -1,6 +1,7 @@
 export default [
     {
         course: 'BCA',
+        image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
         name: 'Deepanshu',
         ID: '21041329',
         semester: 3,
@@ -8,54 +9,63 @@ export default [
     },
     {
         course: 'BCA',
+        image: 'https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHJhbmRvbSUyMHBlb3BsZXxlbnwwfHwwfHw%3D&w=1000&q=80',
         name: 'Kuldeep',
         ID: '21041350',
         semester: 3
     },
     {
         course: 'BCA',
+        image: 'https://static.generated.photos/vue-static/home/feed/latino-male.png',
         name: 'Nishant',
         ID: '20141360',
         semester: 3
     },
     {
         course: 'BCA',
+        image: 'https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/35af6a41332353.57a1ce913e889.jpg',
         name: 'Harvinder',
         ID: '21041355',
         semester: 3
     },
     {
         course: 'BCA',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSudftFBVS-q2SwkCoDtYq4jvq50Sq604AP2z8RVlen&s',
         name: 'Harnish',
         ID: '21041335',
         semester: 3
     },
     {
         course: 'BCA',
+        image: 'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
         name: 'Manish',
         ID: '21041340',
         semester: 3
     },
     {
         course: 'BCA',
+        image: 'https://this-person-does-not-exist.com/static/images/fake-2.jpg',   
         name: 'Deepak',
         ID: '21041355',
         semester: 3
     },
     {
         course: 'BCA',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8ZfP420ug18r6ZnakyRWOhmExPbrhCV1WR5ds_4U&s',
         name: 'kartik',
         ID: '21041385',
         semester: 3
     },
     {
         course: 'BCA',
+        image: 'https://pbs.twimg.com/media/D8dDZukXUAAXLdY.jpg',
         name: 'karan',
         ID: '21041350',
         semester: 3
     },
     {
         course: 'BCA',
+        image: 'https://i.dailymail.co.uk/i/pix/2013/08/29/article-2405475-1B8389EE000005DC-718_634x550.jpg',
         name: 'Gaurav',
         ID: '21041357',
         semester: 3
